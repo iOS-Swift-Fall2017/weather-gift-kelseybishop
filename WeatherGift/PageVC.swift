9b45f0452b64829b672a6534a02f65535d6445ae
Kelsey Bishop <kelseybishop@Kelseys-MacBook-Air.local>
2017-10-31 16:31:21 -0400

    updated

--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -11,7 +11,7 @@ import UIKit
 class PageVC: UIPageViewController {
     
     var currentPage = 0
-    var locationsArray = ["Local City", "Sydney, Australia", "Accra, Ghana", "Uglich, Russia"]
+    var locationsArray = [WeatherLocation]()
     var pageControl: UIPageControl!
     var listButton: UIButton!
     var barButtonWidth: CGFloat = 44
@@ -19,9 +19,11 @@ class PageVC: UIPageViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         delegate = self
         dataSource = self
+        var newLocation = WeatherLocation()
+        newLocation.name = ""
+        locationsArray.append(newLocation)
         setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil)
     }
     
@@ -43,6 +45,7 @@ class PageVC: UIPageViewController {
         
         pageControl.pageIndicatorTintColor = UIColor.lightGray
         pageControl.currentPageIndicatorTintColor = UIColor.black
+        pageControl.backgroundColor = UIColor.white
         pageControl.numberOfPages = locationsArray.count
         pageControl.currentPage = currentPage
         view.addSubview(pageControl)
@@ -66,6 +69,8 @@ class PageVC: UIPageViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let currentViewController = self.viewControllers?[0] as? DetailVC else {return}
+        locationsArray = currentViewController.locationsArray
         if segue.identifier == "ToListVC" {
             let destination = segue.destination as! ListVC
             destination.locationsArray = locationsArray
@@ -116,6 +121,16 @@ extension PageVC: UIPageViewControllerDelegate, UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
         if let currentViewController = pageViewController.viewControllers?[0] as? DetailVC {
             pageControl.currentPage = currentViewController.currentPage
+        }
+    }
+    
+    @objc func pageControlPressed() {
+        guard let currentViewController = self.viewControllers?[0] as? DetailVC else {return}
+        currentPage = currentViewController.currentPage
+        if pageControl.currentPage < currentPage {
+            setViewControllers([createDetailVC(forPage: pageControl.currentPage)], direction: .reverse, animated: true, completion: nil)
+        } else if pageControl.currentPage > currentPage {
+            setViewControllers([createDetailVC(forPage: pageControl.currentPage)], direction: .forward, animated: true, completion: nil)
         }
     }
     
---
+++ WeatherGift/constants.swift
@@ -0,0 +1,15 @@
+//
+//  constants.swift
+//  WeatherGift
+//
+//  Created by Kelsey Bishop on 10/31/17.
+//  Copyright © 2017 Kelsey Bishop. All rights reserved.
+//
+
+import Foundation
+
+
+let urlBase = "https://api.darksky.net/forecast/"
+
+let urlAPIKey = "3c95c99ce8f27aa8edf04bd86a360f35" + "/"
+
